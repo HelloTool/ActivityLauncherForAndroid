f4c43176b19afc6055fc6b956357ffa059918583
Jesse205 <2055675594@qq.com>
2025-10-11 00:55:24 +0800

    fix(utils):修正Gingerbread主题支持的版本判断
    - 将Gingerbread主题无Bug支持的版本从LOLLIPOP改为ICE_CREAM_SANDWICH
    - 确保在导航手势不支持且SDK版本低于ICS时启用Gingerbread主题
    - 避免在更高版本系统上误用可能存在Bug的主题样式
    
    Signed-off-by: Jesse205 <2055675594@qq.com>

--- app/src/main/java/io/gitee/jesse205/activitylauncher/utils/OSUtils.kt
+++ app/src/main/java/io/gitee/jesse205/activitylauncher/utils/OSUtils.kt
@@ -31,7 +31,8 @@ val isDeviceThemeSupported = Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CR
 val isDeviceSettingsThemeSupported = Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP
 
 
-val isGingerbreadThemeNoBugs = !isNavigationGestureSupported && Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP
+val isGingerbreadThemeNoBugs =
+    !isNavigationGestureSupported && Build.VERSION.SDK_INT < Build.VERSION_CODES.ICE_CREAM_SANDWICH
 
 @get:ChecksSdkIntAtLeast(api = Build.VERSION_CODES.M)
 val isLightStatusBarSupported = Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
